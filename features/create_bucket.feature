f48834f667752c0817de5ea2a5f577314820925c
David Ruan <ruanwz@gmail.com>
2010-09-08 22:53:41 +0800

    add feaure for operating acl

--- features/create_bucket.feature
+++ features/create_bucket.feature
@@ -1,11 +1,29 @@
-Feature: create a bucket
+Feature: bucket
   As a google storeage api user
   I want to create a bucket
   So that I can perform other operation  on the bucket
 
-  Scenario: create an empty bucket
+  Scenario: create, list and delete an empty bucket
     Given the bucket name according the current time
     When I create the bucket
     Then I can list the bucket
+    And the list include the bucket
+    When I delete the bucket
+    Then I can list the bucket
+    And the list doesn't include the bucket
+
+  Scenario: change the acl of an empty bucket
+    Given the bucket name according the current time
+    When I create the bucket
+    Then I can list the bucket
+    And the list include the bucket
+    When I change the acl of the bucket
+    When I get the acl of the bucket
+    Then the acl of bucket is changed
+    When I delete the bucket
+    Then I can list the bucket
+    And the list doesn't include the bucket
+
+
 
 
--- features/step_definitions/gstore.rb
+++ features/step_definitions/gstore.rb
@@ -1,17 +1,44 @@
 When /^the bucket name according the current time$/ do
-  @bucket=Time.now.hash.abs
+  @bucket_name=Time.now.hash.abs.to_s
 end
 
 When /^I create the bucket$/ do
   @client=GStore::Client.new :access_key => $google_storage_api_access_key, :secret_key => $google_storage_api_secret_key
-  @client.create_bucket @bucket
+  @client.create_bucket @bucket_name
 end
 
 Then /^I can list the bucket$/ do
   response= @client.list_buckets
-  puts response
   doc = XmlSimple.xml_in(response)
-  p doc
+  @bucket_name_list=[]
+  doc["Buckets"][0]["Bucket"].each do |b|
+    @bucket_name_list << b["Name"][0]
+  end
 end
+
+Then /^the list include the bucket$/ do
+  @bucket_name_list.should include @bucket_name
+end
+
+When /^I delete the bucket$/ do
+  @client.delete_bucket @bucket_name
+end
+
+Then /^the list doesn't include the bucket$/ do
+  @bucket_name_list.should_not include @bucket_name
+end
+
+When /^I change the acl of the bucket$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+When /^I get the acl of the bucket$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^the acl of bucket is changed$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
 
 
